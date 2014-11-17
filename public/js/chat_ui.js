1574549f44b92fb15de760c8f09df25964d2f3b6
Muhammad Rafdi <muhammad@Muhammads-MacBook-Pro.local>
2014-11-17 01:27:24 +0000

    added more ui handler

--- public/js/chat_ui.js
+++ public/js/chat_ui.js
@@ -6,4 +6,20 @@ function divSystemContentElement(message){
 	return $('<div></div>').html('<i>' + message + '</i>');
 }
 
+function processUserInput(chatApp, socket){
+	var message = $('#send-message').val();
+	var systemMessage;
+
+	if (message.charAt(0) == '/'){
+		systemMessage = chatApp.processCommand(message);
+		if (systemMessage){
+			$('#messages').append(divSystemContentElement(message));
+		}
+	} else {
+		chatApp.sendMessage($('#room').text(), message);
+		$('#messages').append(divEscapedContentElement(message));
+		$('#messages').scrollTop($('#messages').prop('scrollHeight'));
+	}
+	$('#send-message').val('');
+}
 
